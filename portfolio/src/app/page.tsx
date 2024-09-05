62c09cdf70be47ffb5478aa22bf59cc5817032de
tiagogiraldo <santiago.giraldo.e@gmail.com>
2024-09-05 13:54:27 -0500

    Blog markdown add

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,13 +1,9 @@
 import Intro from "@/components/intro";
-import {MDXRemote} from 'next-mdx-remote'
+
 
 export default function Home() {
-    const content =`
-      #This is marksown content
-    `
-
   return (
-    <section className="py-24">
+    <section className="py-20">
       <div className="container max-w-3xl">
         <Intro/>
 
--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
 		center: true,
 		padding: "2rem",
 		screens:{
-			"2xl":"140px",
+			"2xl":"1000px",
 		}
 	},
   	extend: {
